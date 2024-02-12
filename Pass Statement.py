a354428b699216aa4acff08642be2693789c85f8
matiru <gemmatiru@gmail.com>
2024-02-12 17:47:28 +0300

    User Defined Classes

---
+++ Pass Statement.py
@@ -0,0 +1,10 @@
+# Helps write patial statements
+
+favourite = input('Enter your favourite color')
+if favorite == 'red':
+    print('You might like the flag of China')
+elif favourite == 'blue':
+    pass  # fill later '
+
+else:
+    print('You might not like the flag of  China')
---
+++ User Defined Class.py
@@ -0,0 +1,87 @@
+def main():
+    rect1 = create_rectangle(5,10,15,20)
+    rect2 = create_rectangle(10,5,20,15)
+    if rectangles_intersect(rect1,rect2):
+        print('Overlap')
+    else:
+        print('no overlap')
+
+
+def rectangles_intersect(r1,r2):
+    return not ((r1.x + r1.width < r2.x) or (r2.x + r2.width < r1.x) or (r1.y + r1.height < r2.y) or (r2.y + r2.height <r1.y))
+
+
+
+def create_rectangle(corner_x,corner_y,width,height):
+    #component objects
+    rect = Rectangle()# attribute reference
+    rect.x= corner_x
+    rect.y = corner_y
+    rect.width  = width
+    rect.height = height   
+    print(type(rect))    
+    return rect
+class Rectangle: 
+    pass
+
+
+main()
+
+
+
+#Write a class definition for the Person class and write user-defined functions with these function headers:
+from datetime import date
+
+
+def create_person(name, height, birthdate):
+  # Return a a new person object with the given
+  # name, height and birthdate.
+  # - name is a str
+  # - height is an int object in centimetres
+  # - birthdate is a date object from the
+  # module datetime
+    pers= person()
+    pers.name= name
+    pers.height = height
+    person.birthdate = birthdate
+    return pers
+
+class person():
+    pass
+
+def get_age(person):
+    # Return the age of the person in years.
+    today = date.today()
+    age = today.year - person.birthdate.year
+    if (today.month, today.day) < (person.birthdate.month, person.birthdate.day):
+        age -= 1
+
+    return age 
+def get_description(person):
+      # Return a string object of the form: Name is
+      # N cm high and is M years old, where N and M
+      # are integers
+    age  = get_age(person)
+    print(str(person.name) + ' is ' +str(person.height)+' cm high and is ' + str(age)+' years old.')
+    return (str(person.name) + ' is ' +str(person.height)+' cm high and is ' + str(age)+' years old.')
+  
+def main():
+  # Create a person named 'Michael', with height
+  # 190 cm, who was born on August 14, 1976 and
+  # output a description of this individual.
+    
+    #birthdate = date(1976, 8, 14) 
+    name= 'Michael'
+    height = 190
+    birthdate = c_birthdate()
+    birthdate.year = 2000
+    birthdate.month = 1
+    birthdate.day = 19
+    person = create_person(name, height, birthdate)
+    (get_description(person))
+    
+class c_birthdate():
+    pass
+
+main()
+  
